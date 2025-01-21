d65e990c2eb8640e23898353546293f0ef5a069f
Nicolas Posa <nicoposa57@gmail.com>
2025-01-21 01:55:48 -0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Nicolas Posa on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
