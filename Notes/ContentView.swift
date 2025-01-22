3af5ee3e497f0764abcc47b9d8f6f67db11cd87e
Nicolas Posa <nicoposa57@gmail.com>
2025-01-22 01:18:31 -0300

    modifiers

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+//        Text("Este es mi primer codigo en swift ui")
+//            .font(.system(size: 50))
+//            .lineLimit(2)
+//            .multilineTextAlignment(.center)
+//            .bold()
+//            .underline()
+//        Circle()
+//            .background(Color.green)
+//            .padding()
+//            .background(Color.red)
+//            .frame(width: 200, height: 200)
+//        Rectangle()
+//            .frame(width: 200, height: 200)
+//            .foregroundStyle(Color.yellow)
+//            .cornerRadius(50)
+//            .opacity(0.5)
+        Ellipse()
+            .stroke(lineWidth: 5)
+            .frame(width: 200, height: 400)
+            .foregroundStyle(Color.red)
+            .rotationEffect(.degrees(30))
     }
 }
 
