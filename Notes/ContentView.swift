92540b937cc6daa1a566fa819a1cfad6b7252ba6
Nicolas Posa <nicoposa57@gmail.com>
2025-03-27 09:30:08 -0300

    challenge

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color.green
-            Circle()
-            Rectangle()
-                .foregroundStyle(Color.red)
-                .frame(width: 100, height: 100)
-            Text("Hello, World!")
+        
+        VStack{
+            HStack{
+                Text("Hello, world!")
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(10)
+                Text("This is an example of a preview")
+                Image(systemName: "heart")
+                    .foregroundColor(.red)
+            }
+            .padding()
+            .frame(width: 350)
+            .background(Color.blue)
+            .cornerRadius(10)
+            VStack(alignment: .center, spacing: 10){
+                Image(systemName: "heart")
+                    .foregroundStyle(Color.red)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                VStack{
+                    Text("Hello, world!")
+                        .padding(10)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                }
+                VStack{
+                    Text("This is an example of a preview.")
+                }
+                
+            }
+            .padding(20)
+            .frame(width: 350)
+            .background(Color.yellow)
+            .cornerRadius(10)
+            
         }
     }
 }
